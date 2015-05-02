4352c2297db23d2f57567133d3baa5264b21082a
Akamaru <akamaru92@live.de>
2015-05-02 21:22:41 +0200

    Revert "temporary removed source from images plugins"
    This reverts commit bcf65bcaf6554846582071ab2d8cead7e7012eaf.

--- plugins/img_google_nsfw.lua
+++ plugins/img_google_nsfw.lua
@@ -37,8 +37,7 @@ function run(msg, matches)
 
   print("Bilder-URL: ", url)
     send_photo_from_url(receiver, url)
-  --return "Source: "..url
-  return "Bild wird gesendet!"
+	return "Source: "..url
 end
 
 return {
