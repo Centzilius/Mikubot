2c3297f8344619e508e11f03b916a73c8659b05b
Akamaru <akamaru92@live.de>
2015-05-11 20:02:19 +0200

    updated rechner.lua

--- plugins/rechner.lua
+++ plugins/rechner.lua
@@ -1,7 +1,24 @@
-function run(msg, matches)
-  -- Convert expression to a function and execute it
-  local expression = string.gsub(matches[1], "(%a+)", "math.%1")
-  return load('return '..expression)()
+-- Function reference: http://mathjs.org/docs/reference/functions/categorical.html
+
+local function mathjs(exp)
+  local url = 'http://api.mathjs.org/v1/'
+  url = url..'?expr='..URL.escape(exp)
+  local b,c = http.request(url)
+  local text = nil
+  if c == 200 then
+    text = 'Das Ergebnis ist '..b
+  
+  elseif c == 400 then
+    text = b
+  else
+    text = 'Unexpected error\n'
+      ..'Is api.mathjs.org up?'
+  end
+  return text
+end
+
+local function run(msg, matches)
+  return mathjs(matches[1])
 end
 
 return {
