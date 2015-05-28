13f3a0057d9228a02643f0b8c33490ea0b5e4260
Akamaru <akamaru92@live.de>
2015-05-28 21:37:36 +0200

    removed old and added new sticker plugin

---
+++ plugins/sticker.lua
@@ -0,0 +1,17 @@
+function run(msg, matches)
+	if matches[1] == "nairaderp" then
+		send_document(get_receiver(msg), "sticker/naira_derp.webp", ok_cb, false)
+    elseif matches[1] == "facepalm" then
+		send_document(get_receiver(msg), "sticker/facepalm.webp", ok_cb, false)
+    elseif matches[1] == "liste" then
+		return "nairaderp\nfacepalm"
+    end
+end
+
+return {
+    description = "Sendet Custom Sticker", 
+    usage = {"/sticker [Stickername]"},
+    patterns = {"^/[S|s]ticker (.*)$"}, 
+    run = run 
+}
+--by Akamaru [https://ponywave.de]
