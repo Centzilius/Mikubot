7c62730cd476914cf834b4f195b11f5a2adbbcde
Akamaru <akamaru92@live.de>
2015-06-06 22:15:05 +0200

    improved afk plugin
    removed wd plugin

--- plugins/afk.lua
+++ plugins/afk.lua
@@ -1,12 +1,25 @@
 function run(msg, matches)
+  text = matches[2]
   local user_name = get_name(msg)
-  return user_name .. ' ist nun AFK'
+  
+	if matches[1] == '/afk' and not text then
+	  return user_name .. ' ist nun AFK'
+	end
+	
+	if matches[1] == '/afk' then
+      return user_name .. ' ist nun AFK (' .. text .. ')'
+	end
+	
+    if matches[1] == '/re' or '/wd' then
+	  return  user_name .. ' ist wieder da!'
+	end
+	
 end
 
 return {
-    description = "Sagt dass ihr afk seid",
-    usage = {"/afk"},
-    patterns = {"^/afk(.*)$"}, 
-    run = run
+  description = 'Sagt dass ihr afk oder wieder da seid',
+    usage = {'/afk','/re','/wd'},
+  patterns = {'^/([A|a][F|f][K|k])$','^/([A|a][F|f][K|k]) (.*)$','^/([R|r][E|e])$','^/([W|w][D|d])$'}, 
+  run = run 
 }
 --by Akamaru [https://ponywave.de]
