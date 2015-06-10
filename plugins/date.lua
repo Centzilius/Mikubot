bc044b3e44fbf7ad7d58f4a8181779cee1696bdf
Akamaru <akamaru92@live.de>
2015-06-10 19:02:41 +0200

    added time to date.lua

--- plugins/date.lua
+++ plugins/date.lua
@@ -1,9 +1,9 @@
 function run(msg, matches)
-   return os.date("%d.%m.%Y")
+   return os.date("Heute ist der %d.%m.%Y und es ist %H:%M:%S")
 end
 
 return {
-    description = "Zeigt das aktuelle Datum an", 
+    description = "Zeigt das aktuelle Datum und Zeit an", 
     usage = {"/date"},
     patterns = {"^/date$"}, 
     run = run 
