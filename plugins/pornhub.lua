d3c7322222325e985b9015ee20c8c1d086e1d128
Akamaru <akamaru92@live.de>
2015-06-15 14:59:55 +0200

    fixed pornhub and youporn plugin
    thx @synk0

--- plugins/pornhub.lua
+++ plugins/pornhub.lua
@@ -1,48 +1,39 @@
-function run(msg, matches)
+local function googlethat(query)
+  query = "site:pornhub.com" .. query
+  local api = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
+  local parameters = "q=".. (URL.escape(query))
 
-	-- local g = require "./plugins/google"
-	-- local query = "site:pornhub.com+viewkey+" .. matches[1]
-	-- local new_matches = {query}
+  -- Do the request
+  local res, code = https.request(api..parameters)
+  if code ~=200 then return nil  end
+  local data = json:decode(res)
 
-	-- return g.run(msg, new_matches)
-	local results = findPornhub(matches[1])
-	return results-- build_result(results)
+  local results = {}
+  for key,result in ipairs(data.responseData.results) do
+    table.insert(results, {
+      result.titleNoFormatting,
+      result.unescapedUrl or result.url
+    })
+  end
+  return results
 end
 
-function findPornhub(query)
-	local api = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=site:pornhub.com+viewkey+" .. query:gsub(" ", "+")
-
-	-- Do the request
-	local res, code = https.request(api)
-
-	if code ~=200 then return nil end
-	local data = json:decode(res)
-	local results = data.responseData.results
-
-
-
-	math.randomseed( os.time() )
-	math.random(#results)
-	math.random(#results)
-	local index = math.random(#results)
-
-	local result = results[index].titleNoFormatting .. "\n".. results[index].unescapedUrl .."\n"
-
-	return result
+local function stringlinks(results)
+  local stringresults=""
+  for key,val in ipairs(results) do
+    stringresults=stringresults..val[1].." - "..val[2].."\n"
+  end
+  return stringresults
 end
 
-function build_result(query)
-	local stringresults=""
-	for key,val in ipairs(results) do
-	stringresults=stringresults..val[1].."\n"..val[2].."\n\n"
-	end
-	return stringresults
+local function run(msg, matches)
+  local results = googlethat(matches[1])
+  return stringlinks(results)
 end
 
 return {
     description = "Sendet ein PornHub Video",
-    usage = {"/pornhub [Suchwort]","/ph [Suchwort]"},
-    patterns = {"^/pornhub (.*)$","^/ph (.*)$"}, 
-    run = run 
+    usage = {"/pornhub [Begriff]","/ph [Begriff]"},
+    patterns = {"^/pornhub(.*)$","^/ph(.*)$"},
+    run = run
     }
--- Modified by Akamaru [https://ponywave.de]
--- plugins/youporn.lua
+++ plugins/youporn.lua
@@ -1,47 +1,39 @@
-function run(msg, matches)
+local function googlethat(query)
+  query = "site:youporn.com" .. query
+  local api = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
+  local parameters = "q=".. (URL.escape(query))
 
-	-- local g = require "./plugins/google"
-	-- local query = "site:youporn.com+viewkey+" .. matches[1]
-	-- local new_matches = {query}
+  -- Do the request
+  local res, code = https.request(api..parameters)
+  if code ~=200 then return nil  end
+  local data = json:decode(res)
 
-	-- return g.run(msg, new_matches)
-	local results = findYouporn(matches[1])
-	return results-- build_result(results)
+  local results = {}
+  for key,result in ipairs(data.responseData.results) do
+    table.insert(results, {
+      result.titleNoFormatting,
+      result.unescapedUrl or result.url
+    })
+  end
+  return results
 end
 
-function findYouporn(query)
-	local api = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=site%3Ayouporn.com+" .. query:gsub(" ", "+")
-
-	-- Do the request
-	local res, code = https.request(api)
-
-	if code ~=200 then return nil end
-	local data = json:decode(res)
-	local results = data.responseData.results
-
-
-
-	math.randomseed( os.time() )
-	math.random(#results)
-	math.random(#results)
-	local index = math.random(#results)
-
-	local result = results[index].titleNoFormatting .. "\n".. results[index].unescapedUrl .."\n"
-
-	return result
+local function stringlinks(results)
+  local stringresults=""
+  for key,val in ipairs(results) do
+    stringresults=stringresults..val[1].." - "..val[2].."\n"
+  end
+  return stringresults
 end
 
-function build_result(query)
-	local stringresults=""
-	for key,val in ipairs(results) do
-	stringresults=stringresults..val[1].."\n"..val[2].."\n\n"
-	end
-	return stringresults
+local function run(msg, matches)
+  local results = googlethat(matches[1])
+  return stringlinks(results)
 end
 
 return {
-    description = "Sendet ein YouPorn Video",
-    usage = {"/youporn","/yp"},
-    patterns = {"^/youporn (.*)$","^/yp (.*)$"}, 
-    run = run 
-    }
+	description = "Sendet ein YouPorn Video",
+	usage = {"/youporn [Begriff]","/yp [Begriff]"},
+	patterns = {"^/youporn(.*)$","^/yp(.*)$"},
+	run = run
+}
