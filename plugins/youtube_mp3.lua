c530267130ae7f869f8ecc3aba063b8c5e612b41
Akamaru <akamaru92@live.de>
2015-06-29 21:11:47 +0200

    --no-continue

--- plugins/youtube_mp3.lua
+++ plugins/youtube_mp3.lua
@@ -1,18 +1,19 @@
 -- Requires: (sudo) apt-get install youtube-dl
+-- See https://github.com/rg3/youtube-dl/blob/master/README.md#options
 function run(msg, matches)
   URL = matches[1]
   local receiver = get_receiver(msg)
   if string.match(msg.text, '"') then
 	return 'Vergiss es'
   else
-    text = run_bash("rm tmp/sound.mp3 && youtube-dl -o tmp/sound.mp3 --audio-format mp3 " .. URL)
+    text = run_bash("youtube-dl -o tmp/sound.mp3 --audio-format mp3 --no-continue " .. URL)
 	send_audio(get_receiver(msg), "tmp/sound.mp3", ok_cb, false)
     return URL .. ' wurde gedownloadet und wird jetzt gesendet! \nDas kann je nach Video sehr lang dauern! \nAlso habt Geduld!'
   end
 end
 
 return {
-    description = "Downloadet und sendet die mp3 von einem YouTube-Video", 
+    description = "Downloadet und sendet die MP3 von einem YouTube-Video", 
     usage = {"/mp3 [Link]"},
     patterns = {"^/mp3 (https?://[%w-_%.%?%.:/%+=&]+)$"}, 
     run = run
