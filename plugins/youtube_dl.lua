e81c9b48609af33a8ee411b52f428e12cccb1ad3
Akamaru <akamaru92@live.de>
2015-07-05 19:25:49 +0200

    --no-playlist and --max-filesize does not work anymore

--- plugins/youtube_dl.lua
+++ plugins/youtube_dl.lua
@@ -6,7 +6,7 @@ function run(msg, matches)
   if string.match(msg.text, '"') then
 	return 'Vergiss es'
   else
-    text = run_bash("youtube-dl -o tmp/video.mp4 --no-playlist --no-continue --max-filesize 15m " .. URL)
+    text = run_bash("youtube-dl -o tmp/video.mp4 --no-continue " .. URL)
 	send_video(get_receiver(msg), "tmp/video.mp4", ok_cb, false)
     return URL .. ' wurde gedownloadet und wird jetzt gesendet! \nDas kann je nach Video sehr lang dauern! \nAlso habt Geduld!'
   end
