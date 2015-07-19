22169266c0f6e5a2fd61d9621ec9fcfd54ca980f
Akamaru <akamaru92@live.de>
2015-07-19 18:58:45 +0200

    afk plugin (not really) fixed

--- plugins/afk.lua
+++ plugins/afk.lua
@@ -1,26 +1,29 @@
-
 function run(msg, matches)
-  text = matches[2]
+  --local text = matches[2]
   local user_name = get_name(msg)
   
-	if matches[1] == '/afk' and not text then
-	  return user_name .. ' ist nun AFK'
+	--[[if matches[1] == '/afk' and matches[2] then
+	  return user_name .. ' ist nun AFK (' .. text .. ')'
+	else
+      return user_name .. ' ist nun AFK'
+	end]]
+	
+    if string.starts(msg.text, '/afk') then
+      return user_name .. ' ist nun AFK'
+	--elseif string.starts(msg.text, '/afk') and text == not nil then
+	  --return user_name .. ' ist nun AFK (' .. text .. ')'
 	end
 	
-	if matches[1] == '/afk' then
-      return user_name .. ' ist nun AFK (' .. text .. ')'
-	end
-	
-    if matches[1] == '/re' or '/wd' then
+    if string.starts(msg.text, '/re') or string.starts(msg.text, '/wd') then
 	  return  user_name .. ' ist wieder da!'
-	end
+    end
 	
 end
 
 return {
   description = 'Sagt dass ihr afk oder wieder da seid',
     usage = {'/afk','/re','/wd'},
-  patterns = {'^/([A|a][F|f][K|k])$','^/([A|a][F|f][K|k]) (.*)$','^/([R|r][E|e])$','^/([W|w][D|d])$'}, 
+  patterns = {'^/[A|a][F|f][K|k]$','^/[A|a][F|f][K|k] (.*)$','^/[R|r][E|e]$','^/[W|w][D|d]$'}, 
   run = run 
 }
 --by Akamaru [https://ponywave.de]
