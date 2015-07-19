efe6c82ddc47ff2152b49b66bf4fe25b73558472
Akamaru <akamaru92@live.de>
2015-07-19 18:57:50 +0200

    fixed gifeye.lua

--- plugins/gifeye.lua
+++ plugins/gifeye.lua
@@ -1,12 +1,8 @@
 local function run(msg, matches)
 	local receiver = get_receiver(msg)
 	local url = 'http://i.gifeye.com/'..matches[1]..'.gif'
-	  if not file_path then
-		return 'Kein GIF gefunden'
-	  else
 		print("GIF URL: "..url)
 		send_document_from_url(receiver, url)
-	  end
 end
 
 return {
