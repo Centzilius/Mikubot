ef8dc4050a70506c38953708586bd7a40be9f883
Akamaru <akamaru92@live.de>
2015-07-31 21:44:00 +0200

    Improved stille.lua

--- plugins/stille.lua
+++ plugins/stille.lua
@@ -4,16 +4,16 @@ function run(msg, matches)
   local answers = {'Ja, es ist sehr ruhig hier','Wenn es dir zu still ist, kannst du gerne mit mir reden',
   		     '*SCHREI*','Dann mach doch etwas dagegen!','Möpse?','Vermutlich schlafen alle',
                   'Stört dich die Stille?','Nein, es ist nicht still','Nyu?','Besser als Spam','Stille ist schön',
-                  'Mein Name ist Mikubot','Spiel doch etwas http://curvefever.com/',
-                  'Stille sagt manchmal mehr als tausend Worte',
+                  'Mein Name ist Mikubot','Spiel doch etwas Curvefever.com 😉',
+                  'Stille sagt manchmal mehr als tausend Worte','Spiel doch etwas Agrar.io 😉',
                   'Stille und Nacht sind untrennbar verwoben. Die Nacht ist die Stille unter den Sternen.'}
   return answers[math.random(#answers)]
 end
 
 return {
   description = "Es ist so still hier",
-  usage = {"Stille","stille","*stille*","*Stille*"},
-  patterns = {"^Stille","^stille","^*stille*","^*Stille*"},
+  usage = {"Stille","*Stille*"},
+  patterns = {"^[Ss][Tt][Ii][Ll][Ll][Ee]$","^(*)[Ss][Tt][Ii][Ll][Ll][Ee](*)$"},
   run = run
 }
 end
