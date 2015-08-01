f4b7f3f5fe06725aca73c5c85a59643450e27a33
Akamaru <akamaru92@live.de>
2015-08-01 13:26:40 +0200

    more answers for stille.lua

--- plugins/stille.lua
+++ plugins/stille.lua
@@ -2,11 +2,12 @@ do
 
 function run(msg, matches)
   local answers = {'Ja, es ist sehr ruhig hier','Wenn es dir zu still ist, kannst du gerne mit mir reden',
-  		     '*SCHREI*','Dann mach doch etwas dagegen!','Möpse?','Vermutlich schlafen alle',
+				  '*SCHREI*','Dann mach doch etwas dagegen!','Möpse?','Vermutlich schlafen alle',
                   'Stört dich die Stille?','Nein, es ist nicht still','Nyu?','Besser als Spam','Stille ist schön',
                   'Mein Name ist Mikubot','Spiel doch etwas Curvefever.com 😉',
                   'Stille sagt manchmal mehr als tausend Worte','Spiel doch etwas Agrar.io 😉',
-                  'Stille und Nacht sind untrennbar verwoben. Die Nacht ist die Stille unter den Sternen.'}
+                  'Stille und Nacht sind untrennbar verwoben. Die Nacht ist die Stille unter den Sternen.',
+				  'Hier könnte Ihre Werbung stehen!','Folge mir auf Twitter! Twitter.com/Mikubot_'}
   return answers[math.random(#answers)]
 end
 
