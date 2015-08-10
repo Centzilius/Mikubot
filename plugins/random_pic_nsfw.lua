b2a0a441e466ba0162313c6d5358abb9023e2dfb
Akamaru <akamaru92@live.de>
2015-08-10 15:18:55 +0200

    new plugin random_pic_nsfw.lua

---
+++ plugins/random_pic_nsfw.lua
@@ -0,0 +1,35 @@
+do
+function get_random_image(dir)
+  files = scandir(dir)
+  file = files[math.random(#files)]
+	return file
+end
+	 
+function run(msg, matches)
+  local pics = {
+		-- add more below!
+		["/nsfw"] = "/media/AA05-A04B/nsfw/"
+		--["/sfw"] = "/media/AA05-A04B/sfw/"
+		}
+
+  local receiver = get_receiver(msg)
+  local imgtype = matches[1]
+
+  if pics[imgtype] then
+  local img = pics[imgtype]..get_random_image(pics[imgtype])
+	print("Sende... "..img)
+  if string.ends(img, ".gif") then
+    send_document(receiver, img, function() end, function() end)
+  else
+    send_photo(receiver, img, function() end, function() end)
+  end
+  end
+end
+	 
+return {
+    description = "Sendet ein zufälliges Bild (NSFW)", 
+    usage = {"/nsfw"},
+    patterns = {"^/nsfw$"}, 
+    run = run 
+}
+end
