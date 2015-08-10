fd30578acd7ae4b33c2fb27f5da4a9ecb964ef8f
Akamaru <akamaru92@live.de>
2015-08-10 15:17:08 +0200

    more yolos

--- plugins/yolo.lua
+++ plugins/yolo.lua
@@ -1,6 +1,7 @@
 function run(msg, matches)
   local yolos = {'You Only Live Online','You Only Live Once',
-  			   'You Only Love Oppai','You Only Lag Online'
+  			   'You Only Love Oppai','You Only Lag Online',
+  			   'You Only Love Oma','You Only Love Obama'
   			   }
   return yolos[math.random(#yolos)]
 end
