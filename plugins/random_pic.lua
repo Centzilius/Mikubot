8aab9b3ec8d81f67b0e95afcb11e1305499c8d22
Akamaru <akamaru92@live.de>
2015-08-15 15:42:49 +0200

    new plugin random_pic.lua

---
+++ plugins/random_pic.lua
@@ -0,0 +1,35 @@
+do
+function get_random_image(dir)
+  files = scandir(dir)
+  file = files[math.random(#files)]
+	return file
+end
+	 
+function run(msg, matches)
+  local pics = {
+		-- add more below!
+		["anime"] = "/media/AA05-A04B/sfw/anime/",
+		["mlp"] = "/media/AA05-A04B/sfw/mlp/"
+		}
+
+  local receiver = get_receiver(msg)
+  local imgtype = matches[1]
+
+  if pics[imgtype] then
+  local img = pics[imgtype]..get_random_image(pics[imgtype])
+	print("Sende... "..img)
+  if string.ends(img, ".gif") then
+    send_document(receiver, img, function() end, function() end)
+  else
+    send_photo(receiver, img, function() end, function() end)
+  end
+  end
+end
+	 
+return {
+    description = "Sendet ein zufälliges Bild", 
+    usage = {"/rpic [Thema]","Themen:","anime","mlp"},
+    patterns = {"^/rpic (.*)$"}, 
+    run = run 
+}
+end
