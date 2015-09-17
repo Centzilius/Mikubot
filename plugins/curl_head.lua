1b500fbd66e1f6e37960390c76f2f6591cc75e4f
Akamaru <akamaru92@live.de>
2015-09-17 14:10:15 +0200

    curl_head.lua: added dig command

--- plugins/curl_head.lua
+++ plugins/curl_head.lua
@@ -5,8 +5,11 @@ function run(msg, matches)
 	return 'Vergiss es'
   end
   
-  if string.match(msg.text, '/[Hh][Ee][Aa][Dd]') then
+  if string.match(msg.text, '[Hh][Ee][Aa][Dd]') then
     text = run_bash('curl --head --insecure ' .. URL)
+    send_msg(receiver, text, ok_cb, false)
+  elseif string.match(msg.text, '[Dd][Ii][Gg]') then
+    text = run_bash('dig ' .. URL .. ' ANY')
     send_msg(receiver, text, ok_cb, false)
   end
 
@@ -15,6 +18,7 @@ end
 return {
     description = "Führt Befehle in der Konsole aus", 
     usage = {""},
-    patterns = {"^/[Hh][Ee][Aa][Dd] (https?://[%w-_%.%?%.:/%+=&]+)$"},
+    patterns = {"^/[Hh][Ee][Aa][Dd] (.*)$","^/[Dd][Ii][Gg] (.*)"},
     run = run
 }
+--by Akamaru [https://ponywave.de]
